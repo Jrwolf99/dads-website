2b391b356e438782d4c319bf9c99413a6a8dcebf
Jonathan <jonathan.wolf@showplacehq.com>
2025-05-05 16:42:03 -0500

    adjustments

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,9 @@ export default function RootLayout({ children }) {
             {children}
           </main>
           <footer className="flex flex-col items-center justify-center w-full h-24 border-t mt-16 mb-8 pt-20 pb-24 gap-4">
-            <p>© {new Date().getFullYear()} Wolf Music Productions. All rights reserved.</p>
+            <p className="px-5 text-center">
+              © {new Date().getFullYear()} Wolf Music Productions. All rights reserved.
+            </p>
             <p className="text-sm text-gray-500">
               Website designed and made by{' '}
               <a
